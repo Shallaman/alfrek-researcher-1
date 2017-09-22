0684406281259ef803593a274688597859c1c663
Maximilian Gustafsson <maxrgus@Maximilians-MacBook-Pro.local>
2017-09-22 14:59:59 +0200

    primitive header component

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>This is the header</h1>
+  <header class="site_header">
+    <router-link to="/" class="site_logo">
+      alfrek
+    </router-link>
+    <nav class="header_navigation">
+      <ul>
+        <li>
+          <router-link to="/editor">Editor</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
